7b3e45e4938f40bcf7bff35e8eda903e8003dc57
AntonioDattolo <antonio19794.a@gmail.com>
2024-06-24 14:37:18 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+include __DIR__ . "/model/####.php";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/vue@3.4.27/dist/vue.global.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <title>#####</title>
+</head>
+
+<body class="myBg">
+    <header class="container">
+        <h1>OOP-2</h1>
+    </header>
+    <main class="container">
+        <section>
+            <ul>
+                <h5>###</h5>
+                <?php foreach ($array as $element) { ?>
+                    <li><?= $element ?></li>
+                <?php } ?>
+            </ul>
+        </section>
+    </main>
+    <!-- <script src="./js/main.js"></script> -->
+</body>
+
+</html>
---
+++ model/model.php
@@ -0,0 +1,20 @@
+<?php
+class ####
+{
+    public $;
+    public $;
+    public $;
+    public $;
+
+     public function print()
+     {
+         return 
+    }
+
+    public function __construct($, $, $, $){
+        $this-> = $;
+        $this-> = $;
+        $this-> = $;
+        $this-> = $;
+    }
+}
